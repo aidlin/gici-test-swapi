70fe668d54b9bb11da415cae161432cad598507f
Vadim Aidlin <poshtova.skrynka.n@gmail.com>
2018-02-07 18:25:01 +0200

    Implemented basic layout boilerplate and routing.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { FilmsComponent } from './components/films/films.component';
+import { CharactersComponent } from './components/characters/characters.component';
+import { FavoritesComponent } from './components/favorites/favorites.component';
 
+const appRoutes: Routes = [
+  { path: 'films', component: FilmsComponent },
+  { path: 'characters', component: CharactersComponent },
+  { path: 'favorites', component: FavoritesComponent },
+  { path: '', component: HomeComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    FilmsComponent,
+    CharactersComponent,
+    FavoritesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
