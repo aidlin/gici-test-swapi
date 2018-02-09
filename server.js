6a77d101925d517bdd997f8ab653d06b21d7fc8f
Vadim Aidlin <poshtova.skrynka.n@gmail.com>
2018-02-09 07:53:24 +0200

    express routes fix #2

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
-app.use(express.static(__dirname + '/dist'));
-app.listen(process.env.PORT || 8080);
 
+app.use(express.static(__dirname + '/dist'));
 app.get('/*', function(req, res){
     res.sendFile(__dirname + '/index.html');
 })
+
+app.listen(process.env.PORT || 8080);
